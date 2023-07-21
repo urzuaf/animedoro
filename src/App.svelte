5c989ceeac237a0a72921cd45d2ef8e83688c21a
Notand105 <urzua.fernando.f@gmail.com>
2023-07-21 16:33:04 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Timer from './lib/Timer.svelte'
+</script>
+
+<input type="number" name="time" id="selecttime" />
+
+<Timer />
---
+++ src/lib/Timer.svelte
@@ -0,0 +1,20 @@
+
+<script lang="ts">
+   export let time : number = 40 
+   let running : boolean = false 
+
+   const runTime = () =>{
+      let intervak = setInterval(()=>{
+         time -= 1
+         if (time == 0){
+            clearInterval(intervak)
+         }
+      }, 100)
+   }
+
+</script>
+
+<h2>{time}</h2>
+<button on:click={runTime}>run</button>
+
+
